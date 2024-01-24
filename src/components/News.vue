fd2f454a42073142fc9c9ee5887b71a6911323da
jollejonas <jollevb@hotmail.com>
2024-01-24 22:21:36 +0100

    Added News.vue

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,6 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-
+  <div class="relative max-h-400 -z-50">
+    <img
+      src="https://images.unsplash.com/photo-1706038513426-97db705d6bbd?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="News"
+      class="w-full h-auto object-cover max-h-400"
+    />
+    <div class="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center p-4">
+      <div class="text-center">
+        <p class="text-4xl font-bold text-white mb-4">News test</p>
+        <p class="text-2x1 text-white">Something else</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
+  <News />
   <ProductList />
   <ProductList />
 </template>
-  
-  <script setup lang="ts">
-    import ProductList from '../components/ProductList.vue'
 
-  </script>
+<script setup lang="ts">
+import ProductList from '../components/ProductList.vue'
+import News from '../components/News.vue'
+</script>
